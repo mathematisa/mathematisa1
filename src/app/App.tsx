5b0df0c4c4548da4e7e36d217faed839462e5205
mathematisa <damohsin5@gmail.com>
2026-01-02 00:42:27 +0600

    Add files via upload

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,15 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Navbar } from './components/Navbar';
-import { Footer } from './components/Footer';
-import { Home } from './pages/Home';
-import { Animations } from './pages/Animations';
-import { AnimationDetail } from './pages/AnimationDetail';
-import { Courses } from './pages/Courses';
-import { About } from './pages/About';
-import { Contact } from './pages/Contact';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Navbar } from "./components/Navbar";
+import { Footer } from "./components/Footer";
+import { Home } from "./pages/Home";
+import { Animations } from "./pages/Animations";
+import { AnimationDetail } from "./pages/AnimationDetail";
+import { Marketplace } from "./pages/Marketplace";
+import { Dashboard } from "./pages/Dashboard";
+import { CreatePost } from "./pages/CreatePost";
+import { Courses } from "./pages/Courses";
+import { About } from "./pages/About";
+import { Contact } from "./pages/Contact";
 
 export default function App() {
   return (
@@ -16,7 +19,14 @@ export default function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/animations" element={<Animations />} />
-          <Route path="/animations/:id" element={<AnimationDetail />} />
+          <Route
+            path="/animations/:id"
+            element={<AnimationDetail />}
+          />
+          <Route path="/marketplace" element={<Marketplace />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/dashboard/*" element={<Dashboard />} />
+          <Route path="/dashboard/create" element={<CreatePost />} />
           <Route path="/courses" element={<Courses />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
